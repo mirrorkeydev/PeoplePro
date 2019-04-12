3366fc01217287fced3530ea502941807ae9cd1b
mirrorkeydev <gutzmanm@oregonstate.edu>
2019-04-12 14:35:21 -0700

    partial ajax form functionality for Employees

--- PeoplePro2/wwwroot/js/site.js
+++ PeoplePro2/wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(function () {
+    var placeholderElement = $('#modal-placeholder');
+    $('button[data-toggle="ajax-modal"]').click(function (event) {
+        console.log("You pressed the button!")
+        var url = $(this).data('url');
+        $.get(url).done(function (data) {
+            placeholderElement.html(data);
+            placeholderElement.find('.modal').modal('show');
+        });
+    });
+
+    placeholderElement.on('click', '[data-save="modal"]', function (event) {
+        event.preventDefault();
+
+        var form = $(this).parents('.modal').find('form');
+        var actionUrl = form.attr('action');
+        var dataToSend = form.serialize();
+
+        $.post(actionUrl, dataToSend).done(function (data) {
+            placeholderElement.find('.modal').modal('hide');
+        });
+    });
+});
